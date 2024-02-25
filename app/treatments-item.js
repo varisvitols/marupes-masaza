dc9354b651108cd5d1e6c469cc88c30b6fa6305d
Varis Vitols <varisvitols@gmail.com>
2024-02-25 22:32:20 +0200

    Use CSS modules for Treatments item

--- app/treatments-item.js
+++ app/treatments-item.js
@@ -1,13 +1,15 @@
+import styles from './treatments-item.module.css'
+
 export default function TreatmentsItem({ itemData }) {
   const { titleLv, titleEn, price } = itemData
 
   return (
-    <div className="treatments-item">
-      <div className="treatments-item__description">
-        <div className="treatments-item__left">{titleLv}</div>
-        <div className="treatments-item__right">{titleEn}</div>
+    <div className={styles.container}>
+      <div className={styles.description}>
+        <div className={styles.left}>{titleLv}</div>
+        <div className={styles.right}>{titleEn}</div>
       </div>
-      <div className="treatments-item__price">{price}</div>
+      <div className={styles.price}>{price}</div>
     </div>
   )
 }

b5d9a0b996356100aaa299ce50a268628c42cd6a
Varis Vitols <varisvitols@gmail.com>
2024-02-25 19:23:07 +0200

    Introduce ESLint and Prettier code formatter

--- app/page.js
+++ app/page.js
@@ -1,19 +1,18 @@
-import Image from "next/image";
-import TreatmentsItem from "./treatments-item";
-import styles from "./page.module.css";
-import { promises as fs } from 'fs';
+import Image from 'next/image'
+import TreatmentsItem from './treatments-item'
+import styles from './page.module.css'
+import { promises as fs } from 'fs'
 
 export default async function Home() {
-  const file = await fs.readFile(process.cwd() + '/app/data.json', 'utf8');
-  const data = JSON.parse(file);
+  const file = await fs.readFile(process.cwd() + '/app/data.json', 'utf8')
+  const data = JSON.parse(file)
 
   const treatments = data.treatments.map((item) => (
-<TreatmentsItem key={Math.round(Math.random)} itemData={item} />
+    <TreatmentsItem
+      key={Math.round(Math.random)}
+      itemData={item}
+    />
   ))
 
-  return (
-    <main className="treatments">
-      {treatments}
-    </main>
-  );
+  return <main className="treatments">{treatments}</main>
 }

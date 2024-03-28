734d7f366cf106d573a65d6d6001ed1f5bc8a777
Varis Vitols <varisvitols@gmail.com>
2024-03-28 16:59:05 +0200

    Relocate and refactor treatments-item component

--- src/app/[locale]/procedures/page.js
+++ src/app/[locale]/procedures/page.js
@@ -1,17 +1,26 @@
-import TreatmentsItem from '../../treatments-item'
+import TreatmentsItem from '@/src/app/[locale]/_components/treatments-item'
 import { promises as fs } from 'fs'
 
 export default async function Home({ params: { locale } }) {
   const file = await fs.readFile(process.cwd() + '/src/data.json', 'utf8')
   const data = JSON.parse(file)
 
-  const treatments = data.treatments.map((item) => (
-    <TreatmentsItem
-      key={Math.round(Math.random)}
-      itemData={item}
-      lang={locale}
-    />
-  ))
+  const treatments = data.treatments.map((item) => {
+    const currency = 'eur'
+    const title = item.title[locale]
+    const price = item.price[currency]
+    const itemData = {
+      title,
+      price,
+    }
+
+    return (
+      <TreatmentsItem
+        key={Math.round(Math.random)}
+        itemData={itemData}
+      />
+    )
+  })
 
   return <main className="treatments">{treatments}</main>
 }

26b04fec47bb564d3b3b2883a7bc7fc557606919
Varis Vitols <varisvitols@gmail.com>
2024-04-18 09:35:20 +0300

    Add a couple of images to the procedures page

--- src/app/[locale]/procedures/page.tsx
+++ src/app/[locale]/procedures/page.tsx
@@ -5,6 +5,9 @@ import TreatmentsItem from './components/treatments-item'
 import * as path from 'path'
 import { promises as fs } from 'fs'
 import styles from './procedures.module.css'
+import Image from 'next/image'
+import img2 from '@/public/gallery/gallery2.jpeg'
+import img13 from '@/public/gallery/gallery13.jpeg'
 
 export default async function Procedures({ params: { locale } }: PageProps) {
   const file = await fs.readFile(path.join(process.cwd(), 'data.json'), 'utf8')
@@ -31,5 +34,22 @@ export default async function Procedures({ params: { locale } }: PageProps) {
     }
   )
 
-  return <main className={styles.treatments}>{treatments}</main>
+  return (
+    <main className={styles.treatments}>
+      {treatments}
+      <div>
+        <Image
+          src={img2}
+          alt="Procedure"
+          style={{ marginBottom: '2rem' }}
+        />
+      </div>
+      <div>
+        <Image
+          src={img13}
+          alt="Procedure"
+        />
+      </div>
+    </main>
+  )
 }

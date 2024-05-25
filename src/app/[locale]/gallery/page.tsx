cc14064c65decc0fbeefd5879a10c6fa184fca2d
Varis Vitols <varisvitols@gmail.com>
2024-05-25 07:45:45 +0300

    Limit gallery width

--- src/app/[locale]/gallery/page.tsx
+++ src/app/[locale]/gallery/page.tsx
@@ -1,9 +1,10 @@
 import type { PageProps } from '@/.next/types/app/[locale]/page'
 import Gallery from './components/gallery'
+import styles from './gallery.module.css'
 
 export default async function GalleryPage({ params: { locale } }: PageProps) {
   return (
-    <main>
+    <main className={styles.gallery}>
       <div>
         <Gallery />
       </div>
